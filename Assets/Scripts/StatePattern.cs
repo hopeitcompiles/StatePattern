a2037627f2888c5868407862f0ccad58be76b7fb
ilostmycode <cristhian.castro.lucas@udla.edu.ec>
2021-06-07 21:48:40 -0500

    Initial commit

---
+++ Assets/Scripts/StatePattern.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatePattern : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody rb;
+    public StatePattern _state;
+    public State current;
+    public IdleState idlestate;
+    public DancingState dancingstate;
+    public string stringState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim= GetComponent<Animator>();
+        current = new IdleState();
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            current.changeState(this);
+            current.Handle(this);
+        }
+        
+    }
+
+    public interface  State
+    {
+
+        void changeState(StatePattern npc);
+        public void Handle(StatePattern npc);
+    }
+
+    public class DancingState : State
+    {
+        public void changeState(StatePattern npc)
+        {
+            
+            npc.current = new IdleState();
+           
+        }
+        public void Handle(StatePattern npc)
+        {
+            npc.anim.SetBool("dancing", true);
+            Debug.Log("Bailando");
+            
+        }
+
+    }
+    public class IdleState : State
+    {
+        public void changeState(StatePattern npc)
+        {
+            
+            npc.current=new DancingState();
+        }
+        public void Handle(StatePattern npc)
+        {
+            npc.anim.SetBool("dancing", false);
+            Debug.Log("Idle state");
+        }
+
+    }
+
+}
